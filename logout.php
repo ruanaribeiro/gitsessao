e45e989f8fbdb98da082a8cba3acba54de17dbe9
ruanaribeiro <ruanaribeiro3@gmail.com>
2023-10-16 11:37:53 -0300

    commit: criação do arquivo logout.php

---
+++ logout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="p-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>logout</title>
+</head>
+<body>
+    
+</body>
+</html>
